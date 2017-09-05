06e26ecb6bfb5ad15e6245a76bce67f1bf62016d
Ezequiel Falcón <falconezequiel@gmail.com>
2017-09-05 17:54:22 -0300

    objeto de roles en verUsuarios

--- sistema/usuarios.js
+++ sistema/usuarios.js
@@ -22,7 +22,24 @@ module.exports = function (db) {
             db.manyOrNone('select nombre, nombre_apellido, email, telefono, direccion ' +
               'from usuarios where id_cliente_int = $1;', decoded.cliente)
               .then(usuarios => {
-                res.json({resultado: true, datos: usuarios})
+                let usuariosListos = 0;
+                for (const usuario of usuarios) {
+                  db.manyOrNone('select id, nombre ' +
+                    'from roles inner join roles_por_usuario ON roles.id = roles_por_usuario.id_rol ' +
+                    'INNER JOIN usuarios on roles_por_usuario.usuario = usuarios.nombre ' +
+                    'where roles_por_usuario.usuario = $1 and usuarios.id_cliente_int = $2 ;', [usuario.usuario, decoded.cliente])
+                    .then(rolesUsuario => {
+                      usuario.roles = rolesUsuario;
+                      usuariosListos++;
+                      if (usuariosListos === usuarios.length) {
+                        res.json({resultado: true, datos: usuarios})
+                      }
+                    })
+                    .catch(err => {
+                      console.error(err);
+                      res.status(500).json({resultado: false, mensaje: err.detail})
+                    })
+                }
               })
               .catch(err => {
                 console.error(err);

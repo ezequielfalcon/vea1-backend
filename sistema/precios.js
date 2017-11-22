9d41f314191f9953fa2600fa5a567687353a4a48
Ezequiel Falcón <falconezequiel@gmail.com>
2017-11-22 15:05:37 -0300

    modulo de nuevo precio de venta

--- sistema/precios.js
+++ sistema/precios.js
@@ -6,7 +6,41 @@ module.exports = function (db) {
   module.nuevoPrecio = nuevoPrecio;
 
   function nuevoPrecio(req, res) {
-
+    const token = req.headers['x-access-token'];
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          console.log("Error de autenticación, token inválido!\n" + err);
+          res.status(401).json({
+            resultado: false,
+            mensaje: "Error de autenticación"
+          });
+        }
+        else {
+          const roles = JSON.parse(decoded.roles);
+          if (roles.includes('admin')) {
+            if(req.body.id_producto && req.body.precio) {
+              db.none('INSERT INTO precios_por_producto (id_producto, precio, fecha, id_cliente_int) ' +
+                'VALUES ($1, $2, current_timestamp, $3) RETURNING id;', [req.body.id_producto, req.body.precio, decoded.cliente])
+                .then(nuevoPrecio => {
+                  res.json({resultado: true, id: nuevoPrecio.id})
+                })
+                .catch(err => {
+                  console.error(err);
+                  res.status(500).json({resultado: false, mensaje: err.detail})
+                })
+            } else {
+              res.status(400).json({resultado: false, mensaje: 'Faltan parámetros'})
+            }
+          } else {
+            res.status(403).json({
+              resultado: false,
+              mensaje: 'Permiso denegado!'
+            });
+          }
+        }
+      })
+    }
   }
 
   return module;
